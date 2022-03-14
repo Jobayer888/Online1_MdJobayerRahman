dde64741ac60382c2bc9d915b105fe8fa109d946
Jobayer888 <mrahman192070@bscse.uiu.ac.bd>
2022-03-14 15:32:11 +0600

    git commit

---
+++ appt.php
@@ -0,0 +1,27 @@
+<?php
+
+	$Name = $_GET["Name"];
+
+	$APPT_NAME = $_GET["APPT_NAME"];
+
+    $From_Location = $_GET["From_Location"];
+    $To_location = $_GET["APPT_NAME"];
+    $STATUS = $_GET["STATUS"];
+
+
+	require_once('db_connect.php');
+
+	$connect = mysqli_connect( HOST, USER, PASS, DB )
+
+		or die("Can not connect");
+
+
+
+	mysqli_query( $connect, "INSERT INTO t0 VALUES ( '', '$Name', '$APPT_NAME', $From_Location, $To_location, $STATUS )" )
+
+		or die("Can not execute query");
+
+
+	echo "<p><a href=read.php>READ all records</a>";
+
+?>
---
+++ appt_input.php
@@ -0,0 +1,27 @@
+<h1>Create Record</h1>
+
+
+
+<form method=get action=appt.php>
+
+	Name <input type=text name=f0> <br>
+
+	<p>
+
+	APPT_NAME <input type=text name=f1> <br>
+
+	<p>
+
+	From_Location <input type=text name=f1> <br>
+    <p>
+
+	To_location <input type=text name=f1> <br>
+    <p>
+
+	STATUS <input type=text name=f1> <br>
+    <p>
+
+
+	<input type=submit value=Insert>
+
+</form>
